603d980fd220c77d431c8fdb5b8bef2b145593da
larrabyte <larrabyte@protonmail.ch>
2026-07-08 13:02:25 +1000

    Increase cothread count to 11 to account for bootstrapping

--- src/bsp/generic-lionsos/src/libmicrokitco_opts.h
+++ src/bsp/generic-lionsos/src/libmicrokitco_opts.h
@@ -1,8 +1,8 @@
 #pragma once
 
-/* Cothreads for the CFE_EVS, CFE_SB, CFE_ES, ES_BG_TASK, CFE_TIME, TIME_TONE_TASK, TIME_ONEHZ_TASK and CFE_TBL tasks
+/* Cothreads for the initial, initial bootstrap, CFE_EVS, CFE_SB, CFE_ES, ES_BG_TASK, CFE_TIME, TIME_TONE_TASK, TIME_ONEHZ_TASK and CFE_TBL and LIONSOS_PROXY tasks
  * TODO: Track this file in the build system */
-#define LIBMICROKITCO_MAX_COTHREADS 10
+#define LIBMICROKITCO_MAX_COTHREADS 11
 
 /* 64k of stack size */
 #define COTHREAD_STACK_SIZE 0x10000
